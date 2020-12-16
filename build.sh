8d8f95b2c2363b141f765dff6d0841e8bc92936b
Gonzalo0743 <64326722+Gonzalo0743@users.noreply.github.com>
2020-12-16 09:42:18 -0600

    Updating builld

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+echo "Instalando dependencias"
 npm install
 
 echo "Empezando"
-
 npm run build
 
 echo "Medio terminado"
+docker build . -t isaacsnk/star-wars-server:latest
 
-docker build . -t star-wars-server
+echo "Docker push"
+docker push star-wars-server:latest
 
 echo "Se compilo ak7"
