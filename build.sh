7b6ed6c047106bb4ab74b75c13e0828bfcb0add3
Gonzalo0743 <64326722+Gonzalo0743@users.noreply.github.com>
2020-12-16 10:00:59 -0600

    Fixing echos

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ npm install
 echo "Empezando"
 npm run build
 
-echo "Medio terminado"
+echo "Procesando"
 docker build . -t isaacsnk/star-wars-server:latest
 
 echo "Docker push"
 docker login -u Gonzalo0743 -p $DOCKER_HUB_PASSWORD
 docker push star-wars-server:latest
 
-echo "Se compilo ak7"
+echo "Se compiló"
