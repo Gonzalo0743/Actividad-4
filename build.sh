7c9ee1f46251efba5ebe1111ff80c325a9076d92
Gonzalo0743 <64326722+Gonzalo0743@users.noreply.github.com>
2020-12-16 09:54:03 -0600

    Updating password

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ echo "Medio terminado"
 docker build . -t isaacsnk/star-wars-server:latest
 
 echo "Docker push"
+docker login -u Gonzalo0743 -p $DOCKER_HUB_PASSWORD
 docker push star-wars-server:latest
 
 echo "Se compilo ak7"
